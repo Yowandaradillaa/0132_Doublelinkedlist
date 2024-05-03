221063e2d98920090091579100e552da4f3c9a49
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-03 11:05:59 +0700

    Memasukan catch

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -225,5 +225,9 @@ int main()
                 break; 
             }
         }
+        catch (exception &e)
+        {
+            cout << "Check for the values entered." << endl;
+        }
     }
 }
