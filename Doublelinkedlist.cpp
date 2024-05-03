3afbd4065947fe55d6eab5310557a23bfa359c3b
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-03 10:22:50 +0700

    Memasukan void deletedNode

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -66,7 +66,21 @@ bool search(int rollNo, Node **previous, Node **current)
 {
     *previous = NULL;
     *current = START;
-    
-     
+
+     while (*current != NULL && (*current)->noMhs != rollNo)
+     {
+        *previous = *current;
+        *current = (*current)->next;
+     }
+     return(*current!= NULL);
+}
+
+void deletedNode()
+{
+    Node *previous, *current;
+    int rollNo;
+
+    cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+    cin >> rollNo;
 }
 
