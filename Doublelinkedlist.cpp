6d68bd9b2c4812f23ea7e22228c7a02d2fed7469
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-03 10:15:36 +0700

    Memasukan if else

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -46,7 +46,19 @@ void addNode()
         }
         newNode->next = current;
         newNode->prev = previous;
-        
+
+        if (current != NULL)
+        {
+            current->prev = newNode;
+        }
+        if (previous != NULL)
+        {
+            previous->next = newNode;
+        }
+        else
+        {
+            START = newNode;
+        }
     }
 }
 
