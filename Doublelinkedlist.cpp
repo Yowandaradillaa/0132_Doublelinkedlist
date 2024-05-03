4b14666c396e685153f05473a2a2c81683b509a3
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-03 10:38:38 +0700

    Memasukan while

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -133,7 +133,13 @@ void traverse()
     {
         cout << "\nRecords in ascending order of roll number are: " << endl;
         Node *currentNode = START;
-        
+
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode == currentNode-> next;
+        }
     }
 }
+
 
